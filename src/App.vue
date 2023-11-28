f2c08cc50a1993f78e6bda93f2900b75b5ecf14f
andmatrosov <and.matrosow@ya.ru>
2023-11-29 01:25:43 +0300

    BAZA

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import MyHello from './components/MyHello.vue'
+</script>
+
+<template>
+  <MyHello title="Кроссовки Puma" :price="9500" is-added>
+    <button>BTN</button>
+  </MyHello>
+  <MyHello is-added />
+  <MyHello :price="9500" is-added />
+</template>
+
+<style scoped></style>
